028f361c04aedcc7f79925d79ddb56b08111c691
Franz Kugelmann <franz.kugelmann@elementare-teilchen.de>
2017-01-27 07:50:26 +0100

    clear cache also when page cache group is cleared

--- ext_localconf.php
+++ ext_localconf.php
@@ -8,15 +8,16 @@ $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['proc
 // Add new cache configurations
 // The is_array() check is done to enable administrators to overwrite configuration of caches in LocalConfiguration.php. During bootstrap, any ext_localconf.php is loaded after DefaultConfiguration.php and AdditionalConfiguration.php are loaded, so it is important to make sure that the administrator did not already set any configuration of the extensions cache.
 if (!is_array($GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['etcachetsobjects_db'])) {
-    $TYPO3_CONF_VARS['SYS']['caching']['cacheConfigurations']['etcachetsobjects_db'] = array(
+    $TYPO3_CONF_VARS['SYS']['caching']['cacheConfigurations']['etcachetsobjects_db'] = [
         // we use default backend (database at time of writing)
         'frontend' => 'TYPO3\\CMS\\Core\\Cache\\Frontend\\StringFrontend',
-    );
+	'groups' => ['pages','all'],
+    ];
 }
 
 if (!is_array($GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['etcachetsobjects_transient'])) {
-    $TYPO3_CONF_VARS['SYS']['caching']['cacheConfigurations']['etcachetsobjects_transient'] = array(
+    $TYPO3_CONF_VARS['SYS']['caching']['cacheConfigurations']['etcachetsobjects_transient'] = [
         'backend' => 'TYPO3\\CMS\\Core\\Cache\\Backend\\TransientMemoryBackend',
         'frontend' => 'TYPO3\\CMS\\Core\\Cache\\Frontend\\StringFrontend',
-    );
+    ];
 }
