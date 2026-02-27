756707e615d54ec71be17ffc6006ea4aa51aba00
Richard Klein <richard.klein@elementare-teilchen.de>
2026-02-27 14:27:54 +0100

    Refactor DataHandler hooks to be v13 compatible

--- Classes/Hooks/DataHandler.php
+++ Classes/Hooks/DataHandler.php
@@ -24,55 +24,50 @@ namespace ElementareTeilchen\Etcachetsobjects\Hooks;
  *
  *  This copyright notice MUST APPEAR in all copies of the script!
  ***************************************************************/
+
+use ElementareTeilchen\Etcachetsobjects\Event\CollectCacheTagsToBeClearedEvent;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
+use TYPO3\CMS\Backend\Utility\BackendUtility;
+use TYPO3\CMS\Core\Cache\Frontend\FrontendInterface;
+use TYPO3\CMS\Core\EventDispatcher\EventDispatcher;
+use TYPO3\CMS\Core\Exception\SiteNotFoundException;
+use TYPO3\CMS\Core\Site\SiteFinder;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
 /**
  * tcemain hooks. See ext_localconf.php for activating/deactivating them.
  *
  * @author    Franz Kugelmann <franz.kugelmann@elementare-teilchen.de>
  */
-use TYPO3\CMS\Core\Cache\CacheManager;
-use ElementareTeilchen\Etcachetsobjects\Event\CollectCacheTagsToBeClearedEvent;
-use TYPO3\CMS\Backend\Utility\BackendUtility;
-use TYPO3\CMS\Core\Cache\Frontend\FrontendInterface;
-use TYPO3\CMS\Core\EventDispatcher\EventDispatcher;
-use TYPO3\CMS\Core\Site\Entity\Site;
-use TYPO3\CMS\Core\Site\SiteFinder;
-
 class DataHandler
 {
     public function __construct(
-        private readonly CacheManager $cacheManager,
-        private readonly EventDispatcher $eventDispatcher,
-        private readonly SiteFinder $siteFinder
+        #[Autowire(service: 'cache.etcachetsobjects_db')]
+        protected readonly FrontendInterface $cache,
+        #[Autowire(expression: 'service("extension-configuration").get("etcachetsobjects")')]
+        protected readonly array $extensionConfiguration,
+        protected readonly EventDispatcher $eventDispatcher,
+        protected readonly SiteFinder $siteFinder
     ) {}
-    /*
-    * we need to clear our TS object caches if menu relevant data is saved in pages (title, nav_title, ...)
-     * @param array Changed fields
-     * @param string The table we are working on
-     * @param integer Uid of record
-     * @param \TYPO3\CMS\Core\DataHandling\DataHandler reference to parent object
-     * @return void
+
+    /**
+     * we need to clear our TS object caches if menu relevant data is saved in pages (title, nav_title, ...)
      */
     public function processDatamap_preProcessFieldArray(&$incomingFieldArray, $table, $id, &$parentObj): void
     {
         if ($table == 'pages') {
             //$incomingFieldArray contains all fields: todo: how can we easily check only on some fields?
             //Performance-Relevant? how often are other fields edited?
-            #if (count(array_intersect_key($incomingFieldArray, array('title','nav_title')))) {
+            //if (count(array_intersect_key($incomingFieldArray, array('title','nav_title')))) {
+            //    $this->handleFlushing($id);
+            //}
             $this->handleFlushing($id);
-
-            #}
         }
     }
 
     /**
      * This hook is called for example by list module if deleting a record.
      * At this point the action is not yet done, so for example deleted is still 0.
-     *
-     * @param string new, delete, ...
-     * @param string Table we are working on
-     * @param int Record uid
-     * @param mixed    Unused
-     * @param \TYPO3\CMS\Core\DataHandling\DataHandler Unused reference to parent object
      */
     public function processCmdmap_preProcess($command, $table, $id, $value, &$pObj): void
     {
@@ -81,29 +76,22 @@ class DataHandler
         }
     }
 
-
     /**
      * we have different variants to flush caches
      * - based on PageTSConfig
      * - based on domain related pagetree part
      * - whole tree is relevant (simple, but cache often cleared if many editor changes)
-     * @param $pageId
      */
     private function handleFlushing($pageId): void
     {
-        /** @var FrontendInterface $tsCache */
-        $tsCache = $this->cacheManager->getCache('etcachetsobjects_db');
-        $extConfiguration = $GLOBALS['TYPO3_CONF_VARS']['EXTENSIONS']['etcachetsobjects'];
-
-        switch ($extConfiguration['clearCacheVariant']) {
-
+        switch ($this->extensionConfiguration['clearCacheVariant'] ?? '') {
             case 'PageTS Setting':
                 // if pageId is no int (eg "NEW_1"), we just finish cache flushing
                 if (is_int($pageId) === false) {
                     return;
                 }
-                $pageTSconfig = BackendUtility::getPagesTSconfig($pageId);
-                $tagsToBeFlushed = explode(',', (string) @$pageTSconfig['tx_etcachetsobjects.']['clearByTags']);
+                $pageTSconfig = BackendUtility::getPagesTSconfig($pageId)['tx_etcachetsobjects.']['clearByTags'] ?? '';
+                $tagsToBeFlushed = GeneralUtility::trimExplode(',', $pageTSconfig, true);
 
                 $tagsToBeFlushed = $this->eventDispatcher->dispatch(
                     new CollectCacheTagsToBeClearedEvent($pageId, $tagsToBeFlushed)
@@ -111,8 +99,8 @@ class DataHandler
 
                 $tagsToBeFlushed = array_unique($tagsToBeFlushed);
                 foreach ($tagsToBeFlushed as $cacheTag) {
-                    if ($tsCache->isValidTag($cacheTag)) {
-                        $tsCache->flushByTag($cacheTag);
+                    if ($this->cache->isValidTag($cacheTag)) {
+                        $this->cache->flushByTag($cacheTag);
                     }
                 }
 
@@ -124,17 +112,18 @@ class DataHandler
                     return;
                 }
 
-                $site = $this->siteFinder->getSiteByPageId($pageId);
-                if ($site instanceof Site) {
-                    $tsCache->flushByTag($site->getIdentifier());
+                try {
+                    $site = $this->siteFinder->getSiteByPageId($pageId);
+                    $this->cache->flushByTag($site->getIdentifier());
+                } catch (SiteNotFoundException $e) {
                 }
 
                 break;
 
-                // default is also just flush all
             case 'From all pages':
             default:
-                $tsCache->flush();
+                // default is also just flush all
+                $this->cache->flush();
                 break;
         }
     }
